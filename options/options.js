56937dd486c9189a27aa36662ab7b3c130f90db1
django <django@e59df45c5270>
2024-06-20 20:45:12 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "plain-dream-43",
+  url: "https://plain-dream-43.botics.co",
+  api: "https://plain-dream-43.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
